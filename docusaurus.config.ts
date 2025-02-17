02d0c03b2b442bf81fb5c09256a77e3f1d620b7a
Basia <aktototak@gmail.com>
2025-02-17 15:58:52 +0100

    hide Tutorial

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -74,12 +74,12 @@ const config: Config = {
         src: 'img/logo.png',
       },
       items: [
-        {
-          type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
-          position: 'left',
-          label: 'Tutorial',
-        },
+        //{
+          //type: 'docSidebar',
+          //sidebarId: 'tutorialSidebar',
+          //position: 'left',
+          //label: 'Tutorial',
+        //},
         {to: '/blog', label: 'Blog', position: 'left'},
         {
           href: 'https://github.com/facebook/docusaurus',
